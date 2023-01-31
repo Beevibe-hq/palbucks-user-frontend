af3f528849da37845e91a4b370bc44c2a0d45469
Kelvin <aborkelvin@gmail.com>
2023-01-31 18:05:08 +0100

    Added the description and campaign organizer section in the detailedevents page

---
+++ src/components/campaignorganizers/campaignorganizers.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import arrowdown from "../../images/arrowdown.svg"
+import arrowup from "../../images/arrowup.svg"
+import organizersimg from "../../images/organizers.svg"
+
+
+function Campaignorganizers(props){
+
+    const [openorganizers, setopenorganizers] = useState(false)
+
+    const manageorganizers = () =>{
+
+        if(openorganizers == true){
+            setopenorganizers(false)
+        }else{
+            setopenorganizers(true)
+        }
+    }
+
+    return(
+        <div className = "w-full bg-white" >
+            <div className = "flex justify-between items-center py-5 px-7" >
+            <h3 className = "text-lg font-bold flex gap-2 " >
+                <img src={organizersimg} alt="organizer icon" />
+                <span>Campaign organiser(s)</span>
+            </h3>
+                <img src={openorganizers? arrowup : arrowdown} alt="down arrow" className = " cursor-pointer w-[30px] h-[14px] "
+                    onClick = {manageorganizers}  />
+            </div>
+
+            <hr className = " border-[1px] border-[#C4C4C4] " />
+            
+                <div className ={` ${openorganizers ? 'flex' : 'hidden'} flex-col  mb-3 gap-3 px-7 py-6  `} >
+                {
+                    props.organizerdetails.map((item,i) =>{
+                        return(
+                            <div key ={i} className = "flex items-center gap-2 md:gap-5 "  >
+                                <img src={item.image} alt="organizer account image" className = "w-[60px] md:w-[65px] "  />
+                                {i == 0 ? 
+                                <p className = "font-medium text-base md:text-lg " ><span className = "font-bold" >{item.name}</span> is the organiser of this crowdfund </p> :
+                                <p className = "font-medium text-base md:text-lg " ><span className = "font-bold" >{item.name}</span> is the co-organiser </p>
+                                
+                                }
+                            </div>
+                        )
+                    })
+                }
+                </div>
+                                
+            
+
+        </div>
+
+    )
+}
+
+export default Campaignorganizers
--- src/components/detailedevent/detailedevent.jsx
+++ src/components/detailedevent/detailedevent.jsx
@@ -2,6 +2,10 @@ import { useDispatch, useSelector } from "react-redux"
 import { sethomeorevent } from "../../actions/actions"
 import backarrow from "../../images/backarrow.svg"
 import date from "../../images/date.svg"
+import share from "../../images/share.svg"
+import likeicon from "../../images/likeicon.svg"
+import locateicon from "../../images/locateicon.svg"
+import Campaignorganizers from "../campaignorganizers/campaignorganizers"
 
 function Detailedevent(props){
 
@@ -17,8 +21,37 @@ function Detailedevent(props){
     let eventid = propdetails.charAt(propdetails.length-1) //This cuts out the id of the selected fundevent from the props
     let eventdetails  = homebodydata[eventid] // Uses the above id to select the clicked event's details from redux store
 
+    
+    var organizerdetails = []
+    function arrangeorganizerdetails(){
+        
+        /* organizerdetails = [
+            {
+                image: eventdetails.organizeraccounts[0],
+                name:eventdetails.organizeraccounts[0],
+            },
+            {
+                image: eventdetails.organizeraccounts[1],
+                name:eventdetails.organizeraccounts[1],
+            }
+        ] */
+        
+        //Puts organizerdetails in the above structure so it can be accessed easily in campaignorganizers component
+        for(let i = 0; i<eventdetails.organizeraccounts.length; i++ ){
+            
+            let organizer = {
+                name:eventdetails.organizeraccounts[i],
+                image:eventdetails.organizerimg[i]
+            }
+
+            organizerdetails.push(organizer)
+        }        
+    }    
+    arrangeorganizerdetails()
+    
     //console.log(eventdetails)
     
+    //This converts the total number of people donated from normal numbers to formatted for space
     const formatnumber = n => {
         if (n < 1e3) return n;
         if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
@@ -45,7 +78,7 @@ function Detailedevent(props){
                 <span>{`${eventdetails.days} days remaining`}</span>
             </div>
             
-            <div className="w-full flex flex-col md:flex-row gap-3 md:gap-10 ">
+            <div className="w-full flex flex-col md:flex-row gap-3 md:gap-10 mb-8 ">
                 
                 <div className =" relative w-full md:w-[60%] " >
                     <img src={eventdetails.placeholder} alt="Fund event image" className = {` w-full h-[250px] md:h-[350px] rounded-xl `} />
@@ -77,6 +110,44 @@ function Detailedevent(props){
                     <span className="text-[#2CA9F2] cursor-pointer "> fund using card payment</span>
                     </p>                    
                 </div>
+
+            </div>
+
+            <div className = "w-full md:w-[60%]  pb-10 " >
+
+                <hr className = " border-[1px] border-[#888888] mb-8 " />
+                
+                <button className = "bg-[#2CA9F2] flex text-white gap-2 py-[7px] px-3 rounded-lg mb-8 font-semibold text-base " >
+                    <img src={share} alt="share icon" />
+                    <span>Share campaign</span>
+                </button>
+
+                <h1 className = "font-bold text-[22px] mb-3 " >
+                    {eventdetails.title? eventdetails.title : 'This is the title of the main user’s crowdfunding'}
+                </h1>
+                <p className = "font-normal mb-5 " >This a  complete description for the crowdfunding to aid others fund this particular crowdfunding. 
+                    Users are allowed to read the complete reason why this use is requesting for money from people. 
+                    Why other users read this, if it is totally convincing enough then this user reading it can fund this project.
+                     If not he will move elsewhere to search for where to put their money. This can go on and on and on till its stops.
+                </p>
+
+                
+                <div className='flex gap-[20px] justify-start mb-10 '> 
+                    <button className = "flex items-center justify-center gap-[8px] h-[28px] bg-white rounded-[9px] w-[82px] py-[5px] px-2 text-base text-[#525252] font-semibold " >
+                        <img src={likeicon} alt = 'like icon' className = "w-[16px]" />
+                        <span>Like</span>
+                    </button>
+                    <button className = "flex items-center justify-center gap-[3px] h-[28px] bg-white rounded-[9px] p-3 text-base font-semibold " >
+                        <img src = {locateicon} alt = 'location icon' className = "w-[16px]" />
+                        <span>{eventdetails.location}</span>
+                    </button>
+                </div>
+
+                <hr className = " border-[1px] border-[#888888] mb-8 " />
+
+                <Campaignorganizers organizerdetails = {organizerdetails}  />
+
+
 
             </div>
 
