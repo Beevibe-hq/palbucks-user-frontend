9c71140ef5d53606db5095d1362eef56f522fc25
uche <aborkelvin@gmail.com>
2023-05-16 16:29:11 +0100

    completed notificationspage layout

--- src/pages/notificationspage/notificationspage.jsx
+++ src/pages/notificationspage/notificationspage.jsx
@@ -5,6 +5,8 @@ import Navbar from "../../components/navbar/navbar";
 import backarrow from "../../images/backarrow.svg"
 import campaignimage from "../../images/campaign image.png"
 import useravatar from "../../images/user3.svg"
+import useravatar2 from "../../images/user4.svg"
+import useravatar3 from "../../images/user5.svg"
 import locateicon from "../../images/locateicon2.svg"
 import twittericon from "../../images/twittericon.svg"
 import facebookicon from "../../images/facebookicon.svg"
@@ -14,6 +16,9 @@ import { useNavigate } from "react-router-dom";
 import { setnotificationspageactive, setnotificationspageinactive } from "../../actions/actions";
 import Likenotification from "../../components/likenotification/likenotification";
 import Commentnotification from "../../components/commentnotification/commentnotification";
+import Campaignnotification from "../../components/campaignnotification/campaignnotification";
+import Requestnotification from "../../components/requestnotification/requestnotification";
+import Acceptnotification from "../../components/acceptnotification/acceptnotification";
 
 function Notificationspage(){
 
@@ -52,9 +57,25 @@ function Notificationspage(){
                     <p className=" text-[18px] mb-8 " >See notifications about your crowdfunding campaigns</p>
 
                     <h2 className="text-[20px] font-bold mb-6 " >Today</h2>
+                    <div className="mb-[45px]">
                         <Likenotification userdp = {useravatar} username = 'Carlos' time = '2 hours ago' />
-                        <Likenotification userdp = {useravatar} username = 'Tochi' time = '5 hours ago' />
-                        <Commentnotification userdp = {useravatar} username = 'Tochi' time = '5 hours ago' />
+                        <Commentnotification userdp = {useravatar3} username = 'Tochi' time = '5 hours ago' />
+                        <Campaignnotification time = '12 hours ago' campaignstate = '100%' />
+                        <Requestnotification userdp={useravatar2} username='Franca' time = '13 hours ago' />
+                    </div>
+
+                    <h2 className="text-[20px] font-bold mb-6 " >This week</h2>
+                    <div className="mb-[45px]" >
+                        <Campaignnotification time = '1 day ago' campaignstate = '50%' />
+                        <Acceptnotification username = 'Timothy' userdp={useravatar3} time = '2 days ago' />
+                    </div>
+
+                    <h2 className="text-[20px] font-bold mb-6 " >This month</h2>
+                    <div>
+                        <Likenotification userdp = {useravatar2} username = 'Tochi' time = '2 weeks ago' />
+                    </div>
+
+
                 </div>
             </div>
 
