db4cf0babdd02517a4d2c7d5d00261fe294c8f08
uche <aborkelvin@gmail.com>
2024-12-01 14:58:11 +0100

    Fix vercel chromium

--- patch-react-snap.js
+++ patch-react-snap.js
@@ -1,25 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-const reactSnapPath = path.resolve(
+const filePath = path.resolve(
   __dirname,
-  "node_modules/react-snap/lib/puppeteer_utils.js"
+  "node_modules/react-snap/node_modules/puppeteer/lib/Launcher.js"
 );
 
-const fileContent = fs.readFileSync(reactSnapPath, "utf8");
-
-// Add Puppeteer arguments and fix executablePath for Vercel
-const modifiedContent = fileContent.replace(
-  "const browser = await puppeteer.launch(",
+const content = fs.readFileSync(filePath, "utf8");
+const patchedContent = content.replace(
+  "const browser = await puppeteer.launch();",
   `
-  const chromium = require('chrome-aws-lambda');
-  const browser = await chromium.puppeteer.launch({
-    args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox'],
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath || puppeteer.executablePath(),
-    headless: chromium.headless,
-  });`
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  `
 );
 
-fs.writeFileSync(reactSnapPath, modifiedContent, "utf8");
-console.log("react-snap has been patched for Puppeteer");
+fs.writeFileSync(filePath, patchedContent, "utf8");
